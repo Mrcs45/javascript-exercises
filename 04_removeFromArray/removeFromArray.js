52155a556b2b3c6d9844809bc3184d1e5b40f7c5
Marcos Neto <marcos.msyu@outlook.com>
2025-02-11 15:31:47 -0300

    exercise 04-revomeFromArray done
    This one was a pain, took several hours to understand how filter works

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    return arr.filter(index => !args.includes(index))
 };
 
+    
 // Do not edit below this line
 module.exports = removeFromArray;
