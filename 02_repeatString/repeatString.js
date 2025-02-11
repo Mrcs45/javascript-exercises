184f324ba369f5b0d670ebcc3c39508525566d4b
Marcos Neto <marcos.msyu@outlook.com>
2025-02-11 12:07:12 -0300

    exercise-02_repeatString-done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(value, n) {
+    let result = '';
 
+    if (n < 0) {
+        return 'ERROR'
+    }
+
+    for (let i = 0; i < n; i++) {
+        result += value;
+        
+    }
+    
+    
+    return result;
 };
 
 // Do not edit below this line
