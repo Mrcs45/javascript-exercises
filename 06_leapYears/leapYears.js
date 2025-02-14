4079638c4f203b55ba30ffb0bf6b0f84da6de7a4
Marcos Neto <marcos.msyu@outlook.com>
2025-02-14 16:59:43 -0300

    Exercise 06-leapYears solved
    this one was very funny to do, i think i will forget what is a leap year
    in minutes but i laughed very hard when i see how a solve this versus
    how solution.js did it.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,36 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    
+    /* How to Know if Certain Year is a Leap Year
+The 3 conditions for a given year be a leap year are:
+
+The year is exactly divisible by four (with no remainder);
+If the year is divisible by 100 (years ending in two zeros), it is not a leap, except if
+It is also divisible by 400 (in this case it will be a leap year). */
+
+    const divisibleByFour = year / 4;
+    const divisibleBy100 = year / 100;
+    const divisibleBy400 = year / 400;
+
+    const checkLastTwoDigits = String(year).slice(-2);
+    
+    if (checkLastTwoDigits === '00') {
+        if (Number.isInteger(divisibleByFour) && Number.isInteger(divisibleBy100) && Number.isInteger(divisibleBy400)) {
+            return true;
+        } else {
+            return false;
+        };
+    };
+    
+    
+    if (Number.isInteger(divisibleByFour) && !Number.isInteger(divisibleBy100)) {
+        return true;
+    };
+
+    if (!Number.isInteger(divisibleByFour)) {
+        return false;
+    };
+
+
 
 };
 
